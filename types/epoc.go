80bc28bcaf049a7197b0f6eecc743cdbc6ff7f7d
Vincent Lee <cool8511@gmail.com>
2019-03-24 14:40:59 +0800

    fix ttl which may return never expires

--- types/epoc.go
+++ types/epoc.go
@@ -10,6 +10,10 @@ const (
 	CITRUSLEAF_EPOCH = 1262304000
 )
 
+func ServerTStamp(secsFromCitrusLeafEpoc uint32) int64 {
+	return int64(CITRUSLEAF_EPOCH + secsFromCitrusLeafEpoc)
+}
+
 // TTL converts an Expiration time from citrusleaf epoc to TTL in seconds.
 func TTL(secsFromCitrusLeafEpoc uint32) uint32 {
 	switch secsFromCitrusLeafEpoc {
@@ -17,6 +21,11 @@ func TTL(secsFromCitrusLeafEpoc uint32) uint32 {
 	case 0: // when set to don't expire, this value is returned
 		return math.MaxUint32
 	default:
-		return uint32(int64(CITRUSLEAF_EPOCH+secsFromCitrusLeafEpoc) - time.Now().Unix())
+		ts := ServerTStamp(secsFromCitrusLeafEpoc)
+		now := time.Now().Unix()
+		if ts > now {
+			return uint32(ts - now)
+		}
+		return 1
 	}
 }
